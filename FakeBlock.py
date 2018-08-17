6f9da5b81922afc926eac1115fbcab357f3c0959
Andrew Sentman <asentman.s6@gmail.com>
2018-08-17 11:49:06 -0700

    Compatibility updates to RockBlock simulator

--- FakeBlock.py
+++ FakeBlock.py
@@ -1,12 +1,29 @@
 #!/usr/bin/python
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 import threading
+import urllib
 import time
 import serial
 import sys
 import Queue
 PORT_NUMBER = 8080
 
+
+def sanitize(s):
+    o=""
+    for i in xrange(len(s)):
+        if s[i]=='\r':
+            o=o+'\xEF\xBC\xB2'
+        elif s[i]=='\n':
+            o=o+'\xEF\xBC\xAE'
+        elif ord(s[i])<32:
+            o=o+'\xEF\xBC\x8A'
+        else:
+            o=o+s[i]
+    return o
+
+#rbdebug='\x00\x04\xf0\x9f\x8e\x88\x02\xa5'
+rbdebug=''
 
 thread1=None
 #This class will handles any incoming request from
@@ -21,6 +38,7 @@ class myHandler(BaseHTTPRequestHandler):
         if self.path=='/' or self.path.startswith('/?message='):
             if self.path.startswith('/?message='):
                 msg=self.path.split('=')[1]
+                msg=urllib.unquote(msg)
                 print msg
                 thread1.putmsg(msg)
             self.wfile.write("""
@@ -30,18 +48,26 @@ class myHandler(BaseHTTPRequestHandler):
                     function reloadmsg () {
                         $("#messages").load("messages.txt");
                         $('#messages').scrollTop($('#messages')[0].scrollHeight);
-
+                        $("#messagesh").load("messages.hex");
+                        $('#messagesh').scrollTop($('#messagesh')[0].scrollHeight);
                     }
                 </script>
             </head>
             <body onload="setInterval(reloadmsg,1000)">
-            <textarea cols=80 rows=15 id="messages">Messages</textarea>
-            <form action="/" method="get"><input type="text"name="message"/><input type="submit"/></form>
+            <form accept-charset="UTF-8"><textarea cols=60 rows=30 id="messages">Messages</textarea><textarea cols=120 rows=30 id="messagesh">Messages</textarea></form>
+            <form accept-charset="UTF-8" action="/" method="get"><input type="text"name="message"/><input type="submit"/></form>
             </body>
             """)
         if self.path=='/messages.txt':
             for i in thread1.txes:
-                self.wfile.write(i+'\n')
+                for j in xrange(0,len(i),24):
+                    self.wfile.write(sanitize(i[j:j+24])+'\n')
+                self.wfile.write('-'*24+'\n')
+        if self.path=='/messages.hex':
+            for i in thread1.txes:
+                for j in xrange(0,len(i),24):
+                    self.wfile.write(repr(i[j:j+24])+'\n')
+                self.wfile.write('-'*24*4+'\n')
         return
 
 doRun=1
@@ -76,16 +102,28 @@ class rbthread(threading.Thread):
                         csum=ord(c)<<8
                     elif tmblen==0:
                         csum|=ord(c)
-                        print hex(csum)
-                        print txbuf
+                        print repr(txbuf)
+                        print "Calculated:",hex(sum([ord(i) for i in txbuf]))
+                        print "Received:",hex(csum)
+                        self.send("OK\r\n")
                         state=0
                         continue
             if state==0:
                 if c=='\r':
                     self.send("\r\n")
+                    if cmd=='!Init\'d':
+                        #raw_input('========BREAK========');
+                        print
+                    elif len(cmd)>0 and cmd[0]=='!':
+                        print
+                        pass
                     if cmd=='ATE1':
                         print "Echo on"
                         echo=True
+                        self.send("OK\r\n")
+                    if cmd=='ATE0':
+                        print "Echo off"
+                        echo=False
                         self.send("OK\r\n")
                     elif cmd=='AT&K0':
                         print "Flow control off"
@@ -124,13 +162,14 @@ class rbthread(threading.Thread):
                             self.txes.append(txbuf)
                             txbuf=""
                     elif cmd=='AT+SBDRB':
-                        print "Read RX buffer"
+                        print "Read RX buffer",
                         self.send(chr(len(rxbuf)/256))
                         self.send(chr(len(rxbuf)%256))
                         self.send(rxbuf)
                         csum=sum([ord(i) for i in rxbuf])
-                        self.send(chr((csum/256)%256))
-                        self.send(chr(csum%256))
+                        self.send(chr((csum&0xFF00)>>8))
+                        self.send(chr(csum&0xFF))
+                        self.send("\r\nOK\r\n")
                     elif cmd.startswith('AT+SBDWB='):
                         print "Write TX buffer (bin)"
                         tmblen=int(cmd.split('=')[1])+2
@@ -144,7 +183,9 @@ class rbthread(threading.Thread):
                 else:
                     cmd=cmd+c
     def send(self,msg):
-        self.ser.write(msg)
+        for i in msg:
+            self.ser.write(i)
+            time.sleep(0.01)
     def putmsg(self,msg):
         self.messages.put(msg)
 
